ade9534c42ffb022a5dce52157630f87c4100a49
Lucas Veiga <lucasveigagonzaga@yahoo.com.br>
2022-07-14 18:56:09 -0300

    finalizando parte logica dos posts e comentarios

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Projeto é a construção de um feed de uma rede social chamado Ignite Feed que está sendo construido durante as aulas de ReactJs do Ignite pela Rocketseat
 
 Link do Figma: https://www.figma.com/community/file/1113573231685349036
+Link da aplicação: https://ignite-feed-seven.vercel.app/
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,37 +1,67 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import styles from './Post.module.css'
 
 import { Comment } from './Comment'
-import { Avatar } from './Avatar'
+import { Avatar } from './Avatar'          
+import { useState } from 'react'
 
-export function Post(){
+export function Post({author, publishedAt, content}){
+    const [comments, setComments] = useState([
+        'Post muito bacana'
+    ])
+    const [newCommentText, setNewCommentText] = useState('')
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
+    function handleCreateNewComment(event){
+        event.preventDefault()
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleChangeNewComment(event){
+        setNewCommentText(event.target.value)
+    }
+
     return(
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src='https://avatars.githubusercontent.com/u/99448942?v=4'/>
+                    <Avatar src={author.avatarUrl}/>
                 <div className={styles.authorInfo}>
-                    <strong>Lucas Veiga</strong>
-                    <span>Desenvolvedor front-end</span>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
                 </div>
                 </div>
 
-                <time title="13 de Julho às 15:30h" dateTime="2022-07-13 15:30:00">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p><a href='#'>👉 jane.design/doctorcare</a></p>
-            <p>
-                <a href='#'>#novoprojeto</a>{' '}
-                <a href='#'> #nlw</a>{' '}
-                <a href='#'> #rocketseat</a>
-            </p>
+                {content.map(line => {
+                    if(line.type === 'paragraph'){
+                        return <p>{line.content}</p>
+                    }
+                    return <p><a href="#">{line.content}</a></p>
+                })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
+                    onChange={handleChangeNewComment}
+                    value={newCommentText}
+                    name='comment'
                     placeholder='Deixe um comentário'
                 />
                 <footer>
@@ -40,9 +70,9 @@ export function Post(){
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(comment => {
+                    return <Comment content={comment}/>
+                })}
             </div>
         </article>
     )
